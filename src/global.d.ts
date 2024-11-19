2703adc7ddf3bf410f4eccc411bb5845d1fbcee3
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2024-11-18 19:37:09 -0500

    fix typo

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,5 @@
 declare const __COLOR_MODE_KEY__: 'color-mode'
 declare const __DARK__: 'dark'
 declare const __LIGHT__: 'light'
-declare const __CUSTOM_EVENT_NAME__: 'colorModeChanged'
+declare const __CUSTOM_EVENT_NAME__: 'colorModeChange'
 declare const __BROWSER_OUTPUT__: string
